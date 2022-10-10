139451cbacaab32ab6c45be58697a430f314d92e
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2022-10-10 11:42:46 -0300

    wip

---
+++ 1-TestesUnidade/Demo.Teste/Demo.Teste/02-AssertStringsTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Teste
+{
+    public class AssertStringsTests
+    {
+
+        [Fact]
+        public void StringsTools_UnirNomes_RetornarNomeCompleto()
+        {
+
+            //Arrange
+            var sut = new StringTools();
+            //Act
+            var nomeCompleto = sut.Unir("Marcus", "Fernando");
+            //Assert
+            Assert.Equal("Marcus Fernando", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringTools_DeveIgnorarCase()
+        {
+            //Arrange
+            var sut = new StringTools();
+            //Act
+            var nomeCompleto = sut.Unir("Marcus", "Fernando");
+            //Assert
+            Assert.Equal("MARCUS FERNANDO", nomeCompleto, ignoreCase: true);
+        }
+
+        [Fact]
+        public void StringTools_UnirNomes_DeveConterTrecho()
+        {
+            //Arrange
+            var sut = new StringTools();
+            //Act
+            var nomeCompleto = sut.Unir("Marcus", "Fernando");
+            //Assert
+            Assert.Contains("rnando", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringTools_UnirNomes_DeveComecarCom()
+        {
+            //Arrange
+            var sut = new StringTools();
+            //Act
+            var nomeCompleto = sut.Unir("Marcus", "Fernando");
+            //Assert
+            Assert.StartsWith("Marc", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringTools_UnirNomes_DeveAcabarCom()
+        {
+            //Arrange
+            var sut = new StringTools();
+            //Act
+            var nomeCompleto = sut.Unir("Marcus", "Fernando");
+            //Assert
+            Assert.EndsWith("nando", nomeCompleto);
+        }
+
+
+        [Fact]
+        public void StringTools_UnirNomes_ValidarExpressaoRegular()
+        {
+            //Arrange
+            var sut = new StringTools();
+            //Act
+            var nomeCompleto = sut.Unir("Marcus", "Fernando");
+            //Assert
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", nomeCompleto);
+        }
+
+
+    }
+}
---
+++ 1-TestesUnidade/Demo.Teste/Demo.Teste/04-AssertsNullBoolTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Teste
+{
+    public class AssertsNullBoolTests
+    {
+        [Fact]
+        public void Funcionario_Nome_NaoDeveSerNuloOuVazio()
+        {
+            //Arrange & Act
+            var funcionario = new Funcionario("", salario: 1000);
+
+            //Assert
+            Assert.False(string.IsNullOrEmpty(funcionario.Nome));
+        }
+
+        [Fact]
+        public void Funcionario_Nome_NaoDeveTerApelido()
+        {
+            //Arrange & Act
+            var funcionario = new Funcionario("Marcus", salario: 1000);
+
+            //Assert
+            Assert.True(string.IsNullOrEmpty(funcionario.Apelido));
+
+            //Assert Bool
+            Assert.True(string.IsNullOrEmpty(funcionario.Apelido));
+            Assert.False(funcionario.Apelido?.Length > 0);
+        }
+
+
+
+
+    }
+}
