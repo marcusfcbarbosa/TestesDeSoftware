e6412ea985952f5c18d0efe968f145fbfb2558e1
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2022-10-10 14:25:11 -0300

    Asserts Finalizados

---
+++ 1-TestesUnidade/Demo.Teste/Demo.Teste/06 -AssertingObjectTypesTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Teste
+{
+    public class AssertingObjectTypesTests
+    {
+        [Fact]
+        public void FuncionarioFactory_Criar_DeveRetornarTipoFuncionario()
+        {
+
+            //Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Marcus", salario: 10000);
+            //Assert
+            Assert.IsType<Funcionario>(funcionario);
+        }
+
+        [Fact]
+        public void FuncionarioFactory_Criar_DeveRetornarTipoVariadoPessoa()
+        {
+
+            //Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Marcus", salario: 10000);
+            //Assert
+            Assert.IsAssignableFrom<Pessoa>(funcionario);// ve se ela herda de pessoa
+        }
+    }
+}
---
+++ 1-TestesUnidade/Demo.Teste/Demo.Teste/08 - AssertingExceptionsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Teste
+{
+    public class AssertingExceptionsTests
+    {
+
+        [Fact]
+        public void Calculadora_Dividir_DeveRetornarErroDivisaoPorZero() {
+            //Arrange
+            var calculadora = new Calculadora();
+            //Act& Assert
+            Assert.Throws<DivideByZeroException>(testCode: () => calculadora.Dividir(10, 0));
+        }
+
+        [Fact]
+        public void Funcionario_Salario_DeveRetornarErroSalarioInferiorPermitido() {
+
+            //Arrange & Act & Assert
+            var exception = Assert.Throws<Exception>(testCode: () => FuncionarioFactory.Criar("Marcus", 200));
+            Assert.Equal("Salario inferior ao permitido", exception.Message);
+        }
+
+    }
+}
