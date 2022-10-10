e69db65a8dc97c8165da0dee333dd1a74e6127b2
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2022-10-09 21:27:40 -0300

    Finalizado usdo de Theory e InlineData

--- 1-TestesUnidade/Demo.Teste/Demo.Teste/CalculadoraTests.cs
+++ 1-TestesUnidade/Demo.Teste/Demo.Teste/CalculadoraTests.cs
@@ -17,5 +17,24 @@ namespace Demo.Teste
             Assert.Equal(4, resultado);
             Assert.True(resultado == 4);
         }
+
+        [Theory]
+        [InlineData(2,2,4)]
+        [InlineData(2, 3, 5)]
+        [InlineData(1, 1, 2)]
+        [InlineData(3, 3, 6)]
+        [InlineData(4, 4, 8)]
+        public void Calculadora_Somar_RetornarValoresCorretos(double v1, double v2, double total)
+        {
+            //Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(v1, v2);
+
+            //Assert
+            Assert.Equal(total, resultado);
+            Assert.True(resultado == total);
+        }
     }
 }
