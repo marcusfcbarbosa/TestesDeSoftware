22ddb08495a39da84c9677f0a97beb07fa3b17ef
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2022-10-11 17:25:06 -0300

    Aprendendo a retornar dados via mock .setup

--- Features.Tests/05 - Mock/ClienteServiceTests.cs
+++ Features.Tests/05 - Mock/ClienteServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using Features.Clientes;
 using MediatR;
 using Moq;
+using System.Linq;
 using System.Threading;
 using Xunit;
 
@@ -19,7 +20,8 @@ namespace Features.Tests
 
         [Fact(DisplayName = "Adicionar Cliente com Sucesso")]
         [Trait("Categoria", "Cliente Service Mock Tests")]
-        public void ClienteService_Adicionar_DeveExecutarComSucesso() {
+        public void ClienteService_Adicionar_DeveExecutarComSucesso()
+        {
             //Arrange
             var cliente = _clienteTestsBogusFixture.GerarClienteValido();
             var clienteRepo = new Mock<IClienteRepository>();
@@ -32,7 +34,7 @@ namespace Features.Tests
             Assert.True(cliente.EhValido());//teste inutil
 
             clienteRepo.Verify(r => r.Adicionar(cliente), times: Times.Once);
-            mediator.Verify(m => m.Publish(It.IsAny<INotification>(),CancellationToken.None), times: Times.Once);
+            mediator.Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), times: Times.Once);
 
         }
 
@@ -41,14 +43,39 @@ namespace Features.Tests
         public void ClienteService_Adicionar_DeveExecutarComFalhaDevidoClienteInvalido()
         {
 
+            //Arrange
+            var cliente = _clienteTestsBogusFixture.GerarClienteInvalido();
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediator = new Mock<IMediator>();
+            var clienteService = new ClienteService(clienteRepo.Object, mediator.Object);
+            //Act
+            clienteService.Adicionar(cliente);
+
+            //Assert
+            Assert.False(cliente.EhValido());
+
+            //verifico se os metodos foram chamados
+            clienteRepo.Verify(r => r.Adicionar(cliente), times: Times.Never);
+            mediator.Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), times: Times.Never);
         }
 
-        //[Fact(DisplayName = "Obter Clientes Ativos")]
-        //[Trait("Categoria", "Cliente Service Mock Tests")]
-        //public void ClienteService_Adicionar_DeveExecutarComFaha()
-        //{
-
-        //}
+        [Fact(DisplayName = "Obter Clientes Ativos")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveExecutarComFaha()
+        {
+            //Arrange
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediator = new Mock<IMediator>();
+            //dessa forma ele diz o que deve retornar,pelo Setup quando chamar esse metodo
+            clienteRepo.Setup(c => c.ObterTodos()).Returns(_clienteTestsBogusFixture.ObterClientesVariados());
+            var clienteService = new ClienteService(clienteRepo.Object, mediator.Object);
+            //Act
+            var clientes = clienteService.ObterTodosAtivos();
+            //Assert
+            clienteRepo.Verify(r => r.ObterTodos(), Times.Once);
+            Assert.True(clientes.Any());
+            Assert.False(clientes.Count(c => !c.Ativo) > 0);
+        }
 
 
     }
