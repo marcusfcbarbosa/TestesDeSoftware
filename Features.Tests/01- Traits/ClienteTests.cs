b6742dc5fce2688983ec8687f10a44703d397472
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2022-10-10 21:13:53 -0300

    Traits Finalizadas

---
+++ Features.Tests/01- Traits/ClienteTests.cs
@@ -0,0 +1,53 @@
+﻿using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Features.Tests._01__Traits
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName ="Novo Cliente Válido")]
+        [Trait("Categoria","Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            //Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                nome: "Marcus Fernando",
+                sobrenome: "Correa Barbosa",
+                dataNascimento: DateTime.Now.AddYears(-36),
+                email: "marcus@teste.tco",
+                ativo: true,
+                dataCadastro: DateTime.Now
+                ); ;
+            //Act
+            var result = cliente.EhValido();
+
+            //Assert
+            Assert.True(result);
+            Assert.Equal(expected: 0, actual: cliente.ValidationResult.Errors.Count);
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "edu2edu.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert 
+            Assert.False(result);
+            Assert.NotEqual(0, cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
