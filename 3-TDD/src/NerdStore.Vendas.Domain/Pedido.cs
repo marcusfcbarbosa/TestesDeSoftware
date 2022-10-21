8867f84835b434aca69853e9f12129a6e7cca9f5
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2022-10-21 14:25:15 -0300

    Importando um snipet para facilitar a vida e criando o primeiro teste

---
+++ 3-TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido
+    {
+        public Pedido()
+        {
+            _pedidoItems = new List<PedidoItem>();
+        }
+        public decimal ValorTotal { get; private set; }
+        
+        private readonly List<PedidoItem> _pedidoItems;
+        public IReadOnlyCollection<PedidoItem> PedidoItens => _pedidoItems;
+        public void AdicionarITem(PedidoItem item)
+        {
+            _pedidoItems.Add(item);
+            ValorTotal = PedidoItens.Sum(i=>i.Quantidade * i.ValorUnitario);
+        }
+    }
+    public class PedidoItem
+    {
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
+        {
+            ProdutoId = produtoId;
+            ProdutoNome = produtoNome;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+        }
+    }
+}
---
+++ NerdeStore.Vendas.Domain.Tests/PedidoTests.cs
@@ -0,0 +1,22 @@
+﻿using NerdStore.Vendas.Domain;
+using System;
+using Xunit;
+
+namespace NerdeStore.Vendas.Domain.Tests
+{
+    public class PedidoTests
+    {
+        [Fact(DisplayName = "Adicionar Item Pedido Vazio")]
+        [Trait("Categoria", "Pedido Tests")]
+        public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
+        {
+            // Arrange
+            var pedido = new Pedido();
+            var pedidoItem = new PedidoItem(Guid.NewGuid(),"Produto Teste",2,100);
+            // Act
+            pedido.AdicionarITem(pedidoItem);
+            // Assert
+            Assert.Equal(200, pedido.ValorTotal);
+        }
+    }
+}
